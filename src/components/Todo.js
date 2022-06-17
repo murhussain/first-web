3002c137a1eb058f57cac2163e5c12f205963f5e
murhussain <husseinmurashi@yahoo.com>
2022-06-17 21:00:50 +0200

    complete all react basics now

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,34 @@
+import {useState} from 'react';
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 
 function Todo(props) {
+
+  const [modalIsOpen, setmodalIsOpen] = useState(false);
+
+  function deleteHandler() {
+    setmodalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setmodalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn">Delete</button>
+        <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
+
+      {/* {modalIsOpen ? <Modal /> : null} */}
+      {/* A short hand JS notation */ }
+      { modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/> }
+      { modalIsOpen && <Backdrop onCancel={closeModalHandler} /> }
     </div>
-  )
+  );
 }
 
 export default Todo;
